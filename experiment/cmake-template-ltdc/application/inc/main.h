efa0e5f2d2732155bfda6b712c4bdb2ccb726da0
Rojar Smith <rojarsmith@gmail.com>
2024-04-13 13:58:28 +0800

    feat: Migrate `SystemClock_Config`

--- experiment/cmake-template-ltdc/application/inc/main.h
+++ experiment/cmake-template-ltdc/application/inc/main.h
@@ -1,20 +1,20 @@
 /**
-  ******************************************************************************
-  * @file    BSP/CM7/Inc/main.h
-  * @author  MCD Application Team
-  * @brief   Header for main.c module for Cortex-M7.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2019 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    BSP/CM7/Inc/main.h
+ * @author  MCD Application Team
+ * @brief   Header for main.c module for Cortex-M7.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __MAIN_H
@@ -25,5 +25,8 @@
 //#include "stm32h747xx.h"
 //#include "stm32h7xx_hal_cortex.h"
 #include "stm32h747i_discovery_sdram.h"
+
+/* Exported functions ------------------------------------------------------- */
+void Error_Handler(void);
 
 #endif /* __MAIN_H */
