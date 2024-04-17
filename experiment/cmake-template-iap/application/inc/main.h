ffd7c60ef2b3d2a06886111ab7595eb9e4b2ac6f
Rojar Smith <rojarsmith@gmail.com>
2024-04-17 16:09:42 +0800

    feat: Independent SDK

---
+++ experiment/cmake-template-iap/application/inc/main.h
@@ -0,0 +1,28 @@
+/**
+ ******************************************************************************
+ * @file    main.h
+ * @author  Rojar Smith
+ * @brief   Header for main.c module for Cortex-M7.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 Rojar Smith.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include "stm32h7xx_hal.h"
+#include "stm32h747i_discovery_sdram.h"
+
+#endif /* __MAIN_H */
