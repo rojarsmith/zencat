95b22e34978ec9d97557a214d89b9cffdb1e49bb
Rojar Smith <rojarsmith@gmail.com>
2024-05-02 14:21:30 +0800

    feat: Initial `zencat-stm32-ota-client-app` Project

---
+++ zencat-stm32-ota-client/zencat-stm32-ota-client-app/application/src/main.c
@@ -0,0 +1,328 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Rojar Smith
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 .
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include "main.h"
+
+/* Private define ------------------------------------------------------------*/
+#define FLASH_ADDR_APP 0x08000000
+
+/* Private typedef -----------------------------------------------------------*/
+typedef void (*pFunction)(void);
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+
+//unsigned char __attribute__((section(".fw_info_section_flash"))) fw_info_flash[10] =
+//		{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//unsigned char __attribute__((section(".fw_info2_section_flash"))) fw_info2_flash[10] =
+//		{ 'a', 'b', 'c', 'd' };
+
+/* Private function prototypes -----------------------------------------------*/
+static void MPU_Config(void);
+static void CPU_CACHE_Enable(void);
+static void SystemClock_Config(void);
+static void MX_USART1_UART_Init(void);
+static void Delay_MS(uint32_t ms);
+static void Jump_To_Boot(uint32_t address);
+
+int main(void) {
+	MPU_Config();
+	CPU_CACHE_Enable();
+	HAL_Init();
+	SystemClock_Config();
+
+	MX_USART1_UART_Init();
+
+	BSP_PB_Init(BUTTON_WAKEUP, BUTTON_MODE_GPIO);
+
+	uint32_t
+	address = *(__IO uint32_t*)FLASH_ADDR_APP; // 0x24080000
+	printf("Value at address 0x%08X: 0x%08X\n", (unsigned int) FLASH_ADDR_APP,
+			(unsigned int) address);
+
+	for (;;) {
+		printf("IAP Demo - App\r\n");
+#if (FLASH_ORIGIN == 0x080A0000)
+		printf("Ver B\r\n");
+#endif
+		Delay_MS(3000);
+		if ((BSP_PB_GetState(BUTTON_WAKEUP) == GPIO_PIN_SET)) {
+			printf("Button `Wakeup`\r\n");
+			Jump_To_Boot(address);
+		}
+	}
+}
+
+void Delay_MS(uint32_t ms) {
+	uint32_t tickstart = HAL_GetTick();
+	while ((HAL_GetTick() - tickstart) < ms) {
+		// Empty loop or low power mode
+	}
+}
+
+static void Jump_To_Boot(uint32_t address) {
+	uint32_t jumpAddress;
+	pFunction jumpToApplication;
+
+	// This is testing what is AT the address,
+	// which should be an initial stack pointer,
+	// that needs to be in RAM
+	// The first value stored in the vector table
+	// is the reset value of the stack pointer
+	// AXI SRAM (RAM_D1): 0x24000000~0x2407FFFF(512kb)
+	// 0x24080000 reserved
+	// 2408 0000 = 00100100000010000000000000000000
+	// 2BF7 FFFF = 00101011111101111111111111111111
+	if ((address & 0x2BF7FFFF) == 0x20000000) {
+		//
+		// Disable all interrupts
+		//
+		NVIC->ICER[0] = 0xFFFFFFFF;
+		NVIC->ICER[1] = 0xFFFFFFFF;
+		NVIC->ICER[2] = 0xFFFFFFFF;
+		//
+		// Clear pendings
+		//
+		NVIC->ICPR[0] = 0xFFFFFFFF;
+		NVIC->ICPR[1] = 0xFFFFFFFF;
+		NVIC->ICPR[2] = 0xFFFFFFFF;
+
+		SysTick->CTRL = 0;  // Disable SysTick
+
+		// Set vector table
+		SCB->VTOR = (unsigned long) FLASH_ADDR_APP;
+
+		jumpAddress = *(__IO uint32_t*)(FLASH_ADDR_APP + 4);
+		jumpToApplication = (pFunction) jumpAddress;
+		__set_MSP(*(__IO uint32_t*)FLASH_ADDR_APP);
+		jumpToApplication();
+	}
+}
+
+/**
+ * @brief  Configure the MPU attributes as Write Through for SDRAM.
+ * @note   The Base Address is SDRAM_DEVICE_ADDR.
+ *         The Region Size is 32MB.
+ * @param  None
+ * @retval None
+ */
+static void MPU_Config(void) {
+	MPU_Region_InitTypeDef MPU_InitStruct;
+
+	/* Disable the MPU */
+	HAL_MPU_Disable();
+
+	/* Configure the MPU as Strongly ordered for not defined regions */
+	MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+	MPU_InitStruct.BaseAddress = 0x00;
+	MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
+	MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+	MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+	MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+	MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+	MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+	MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+	MPU_InitStruct.SubRegionDisable = 0x87;
+	MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+
+	HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+	/* Configure the MPU attributes as WT for SDRAM */
+	MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+	MPU_InitStruct.BaseAddress = SDRAM_DEVICE_ADDR;
+	MPU_InitStruct.Size = MPU_REGION_SIZE_32MB;
+	MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+	MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+	MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+	MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+	MPU_InitStruct.Number = MPU_REGION_NUMBER1;
+	MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+	MPU_InitStruct.SubRegionDisable = 0x00;
+	MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+
+	HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+	/* Configure the MPU QSPI flash */
+	MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+	MPU_InitStruct.BaseAddress = 0x90000000;
+	MPU_InitStruct.Size = MPU_REGION_SIZE_128MB;
+	MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+	MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+	MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+	MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+	MPU_InitStruct.Number = MPU_REGION_NUMBER2;
+	MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+	MPU_InitStruct.SubRegionDisable = 0x0;
+	MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+
+	HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+	/* Enable the MPU */
+	HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+}
+
+/**
+ * @brief  CPU L1-Cache enable.
+ * @param  None
+ * @retval None
+ */
+static void CPU_CACHE_Enable(void) {
+	/* Enable I-Cache */
+	SCB_EnableICache();
+
+	/* Enable D-Cache */
+	SCB_EnableDCache();
+}
+
+/**
+ * @brief  System Clock Configuration
+ *         The system Clock is configured as follow :
+ *            System Clock source            = PLL (HSE)
+ *            SYSCLK(Hz)                     = 400000000 (Cortex-M7 CPU Clock)
+ *            HCLK(Hz)                       = 200000000 (Cortex-M4 CPU, Bus matrix Clocks)
+ *            AHB Prescaler                  = 2
+ *            D1 APB3 Prescaler              = 2 (APB3 Clock  100MHz)
+ *            D2 APB1 Prescaler              = 2 (APB1 Clock  100MHz)
+ *            D2 APB2 Prescaler              = 2 (APB2 Clock  100MHz)
+ *            D3 APB4 Prescaler              = 2 (APB4 Clock  100MHz)
+ *            HSE Frequency(Hz)              = 25000000
+ *            PLL_M                          = 5
+ *            PLL_N                          = 160
+ *            PLL_P                          = 2
+ *            PLL_Q                          = 4
+ *            PLL_R                          = 2
+ *            VDD(V)                         = 3.3
+ *            Flash Latency(WS)              = 4
+ * @param  None
+ * @retval None
+ */
+static void SystemClock_Config(void) {
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	HAL_StatusTypeDef ret = HAL_OK;
+
+	/*!< Supply configuration update enable */
+	HAL_PWREx_ConfigSupply(PWR_DIRECT_SMPS_SUPPLY);
+
+	/* The voltage scaling allows optimizing the power consumption when the device is
+	 clocked below the maximum system frequency, to update the voltage scaling value
+	 regarding system frequency refer to product datasheet.  */
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
+	}
+
+	/* Enable HSE Oscillator and activate PLL with HSE as source */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+	RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+	RCC_OscInitStruct.PLL.PLLM = 5;
+	RCC_OscInitStruct.PLL.PLLN = 160;
+	RCC_OscInitStruct.PLL.PLLFRACN = 0;
+	RCC_OscInitStruct.PLL.PLLP = 2;
+	RCC_OscInitStruct.PLL.PLLR = 2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+
+	RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+	RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+	ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+	if (ret != HAL_OK) {
+		Error_Handler();
+	}
+
+	/* Select PLL as system clock source and configure  bus clocks dividers */
+	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK
+			| RCC_CLOCKTYPE_D1PCLK1 | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2
+			| RCC_CLOCKTYPE_D3PCLK1);
+
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+	RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+	ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+	if (ret != HAL_OK) {
+		Error_Handler();
+	}
+
+	/*
+	 Note : The activation of the I/O Compensation Cell is recommended with communication  interfaces
+	 (GPIO, SPI, FMC, QSPI ...)  when  operating at  high frequencies(please refer to product datasheet)
+	 The I/O Compensation Cell activation  procedure requires :
+	 - The activation of the CSI clock
+	 - The activation of the SYSCFG clock
+	 - Enabling the I/O Compensation Cell : setting bit[0] of register SYSCFG_CCCSR
+	 */
+
+	/*activate CSI clock mondatory for I/O Compensation Cell*/
+	__HAL_RCC_CSI_ENABLE();
+
+	/* Enable SYSCFG clock mondatory for I/O Compensation Cell */
+	__HAL_RCC_SYSCFG_CLK_ENABLE();
+
+	/* Enables the I/O Compensation Cell */
+	HAL_EnableCompensationCell();
+}
+
+/**
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART1_UART_Init(void) {
+	huart1.Instance = USART1;
+	huart1.Init.BaudRate = 115200;
+	huart1.Init.WordLength = UART_WORDLENGTH_8B;
+	huart1.Init.StopBits = UART_STOPBITS_1;
+	huart1.Init.Parity = UART_PARITY_NONE;
+	huart1.Init.Mode = UART_MODE_TX_RX;
+	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+	huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+	if (HAL_UART_Init(&huart1) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @param  None
+ * @retval None
+ */
+void Error_Handler(void) {
+	while (1) {
+	}
+}
+
+// printf to uart //
+int _write(int file, char *ptr, int len) {
+	int idx;
+
+	for (idx = 0; idx < len; idx++) {
+		HAL_UART_Transmit(&huart1, (uint8_t*) ptr++, 1, 100);
+	}
+
+	return idx;
+}
