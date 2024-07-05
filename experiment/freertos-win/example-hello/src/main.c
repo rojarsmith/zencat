d4fb5d5d1b2dc0a6f59f2c2289bef9112564870e
Rojar Smith <rojarsmith@gmail.com>
2024-07-05 11:19:34 +0800

    feat: Main Task Pass Struct Data to Background Task with Queue

--- experiment/freertos-win/example-hello/src/main.c
+++ experiment/freertos-win/example-hello/src/main.c
@@ -45,11 +45,19 @@
 
 /*-----------------------------------------------------------*/
 
-static void exampleTask(void *parameters);
+typedef struct
+{
+    int id;
+} DataItem;
+
+static void maTask(void *parameters);
+static void bgTask(void *parameters);
 
 /*-----------------------------------------------------------*/
 
-static void exampleTask(void *parameters)
+QueueHandle_t xQueue;
+
+static void maTask(void *parameters)
 {
     /* Unused parameters. */
     (void)parameters;
@@ -59,7 +67,42 @@ static void exampleTask(void *parameters)
     {
         /* Example Task Code */
         vTaskDelay(100); /* delay 100 ticks */
-        (void)printf("Running %d\n", count);
+        (void)printf("ma Running %d\n", count);
+
+        DataItem item = {count};
+        if (xQueueSend(xQueue, &item, portMAX_DELAY) != pdPASS)
+        {
+            (void)printf("Push to queue error\n");
+        }
+
+        count++;
+    }
+}
+
+static void bgTask(void *parameters)
+{
+    /* Unused parameters. */
+    (void)parameters;
+    long count;
+
+    for (;;)
+    {
+        /* Example Task Code */
+        vTaskDelay(300); /* delay 100 ticks */
+        (void)printf("bg Running %d\n", count);
+
+        UBaseType_t uxNumberOfItems;
+        uxNumberOfItems = uxQueueMessagesWaiting(xQueue);
+
+        for (UBaseType_t i = 0; i < uxNumberOfItems; i++)
+        {
+            DataItem receivedItem;
+            if (xQueueReceive(xQueue, &receivedItem, portMAX_DELAY) == pdTRUE)
+            {
+                (void)printf("bg received id=%d\n", receivedItem.id);
+            }
+        }
+
         count++;
     }
 }
@@ -67,18 +110,34 @@ static void exampleTask(void *parameters)
 
 void main(void)
 {
-    static StaticTask_t exampleTaskTCB;
-    static StackType_t exampleTaskStack[configMINIMAL_STACK_SIZE];
+    xQueue = xQueueCreate(2, sizeof(DataItem));
+    if (xQueue == NULL)
+    {
+        (void)printf("Create queue error\n");
+    }
+
+    static StaticTask_t maTaskTCB;
+    static StackType_t maTaskStack[configMINIMAL_STACK_SIZE];
+    static StaticTask_t bgTaskTCB;
+    static StackType_t bgTaskStack[configMINIMAL_STACK_SIZE];
 
     (void)printf("Example FreeRTOS Project\n");
 
-    (void)xTaskCreateStatic(exampleTask,
-                            "example",
+    (void)xTaskCreateStatic(maTask,
+                            "ma",
                             configMINIMAL_STACK_SIZE,
                             NULL,
                             configMAX_PRIORITIES - 1U,
-                            &(exampleTaskStack[0]),
-                            &(exampleTaskTCB));
+                            &(maTaskStack[0]),
+                            &(maTaskTCB));
+
+    (void)xTaskCreateStatic(bgTask,
+                            "bg",
+                            configMINIMAL_STACK_SIZE,
+                            NULL,
+                            configMAX_PRIORITIES - 2U,
+                            &(bgTaskStack[0]),
+                            &(bgTaskTCB));
 
     /* Start the scheduler. */
     vTaskStartScheduler();
